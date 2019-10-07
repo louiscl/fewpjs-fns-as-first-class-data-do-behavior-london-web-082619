867b7cb7ec4a13e5c1459ec7ca88dcf14a973b8b
github username <github email address>
2019-10-07 16:27:20 +0100

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { endianness } from "os"
+
 /* Given Code, don't edit */
 
 function handleClick(e) {
@@ -7,5 +9,19 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
-/* Write your implementation of greet() */
+function greet(time) {
+  let splitNums = time.split(':')
+  let hour = parseInt(splitNums[0])
+  if(hour < 12) {
+    return 'Good Morning';}
+  else if(hour > 12 && hour < 17){
+    return 'Good Afternoon';}
+  else {
+    return 'Good Evening';}
+}
+
+
 /* Write your implementation of displayMessage() */
+function displayMessage(string) {
+  document.getElementById('greeting').innerText = string;
+}
